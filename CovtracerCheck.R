3e93c7166d561b846692e3d3fd3c9e0ae05c3fe7
tomszosz <42359321+tomszosz@users.noreply.github.com>
2022-03-16 08:26:44 +0100

    222 initial setup (#1)

---
+++ CovtracerCheck.R
@@ -0,0 +1,208 @@
+#!/usr/bin/env Rscript
+
+#
+# script using covtracer R package.
+# based on details from `covr` package.
+# It is a part of covtracer-action on github.
+#
+# Prepare reports:
+# - traceability_matrix
+# - identify untested behaviour
+# - verify directly tested/untested functions
+#
+# Required R packages:
+# - covr - to check coverage
+# - covtracer - to generate reports
+# - optparse - to parse command line options
+# - other packages:
+#   - magrittr - forward pipe operator for R
+#   - dplyr - tool for working with data frame
+#
+
+library("optparse")
+library("magrittr")
+library("covr")
+library("covtracer")
+
+# list of supported arguments
+get_option_list <- function() {
+  list(
+    make_option(
+      "--ignored-file-types",
+      help = "ignored file data types (default %default)",
+      metavar = "ignored-file-types",
+      default = "data,class"
+    ),
+    make_option(
+      "--minimal-coverage",
+      type = "integer",
+      help = "minimal coverage",
+      metavar = "minimal-coverage",
+      default = 80
+    )
+  )
+}
+
+get_arg_parser <- function() {
+  option_list <- get_option_list()
+  OptionParser(usage = "R CMD CovtracerCheck [options] package",
+               option_list = option_list)
+}
+
+usage <- function() {
+  optparse::print_help(get_arg_parser())
+}
+
+parser <- get_arg_parser()
+tryCatch(
+  expr = {
+    arguments <- parse_args(parser, positional_arguments = 1)
+  },
+  error = function(err) {
+    stop("Bad Command Line Option\n", "See './CovtracerCheck.R --help'")
+  }
+)
+
+opt <- arguments$options
+pkg <- arguments$args
+opt$Called_from_command_line <- TRUE # nolint
+
+# print options to log
+message("start-options_list")
+message("pkg: ", pkg, "\n")
+message("options: ")
+minimal_coverage <- opt[["minimal-coverage"]]
+message("minimal_coverage: ", minimal_coverage)
+ignored_file_types <- strsplit(opt[["ignored-file-types"]], ",")
+message("ignored_file_types: ", ignored_file_types)
+message("\nend-options_list\n")
+
+
+curr_wd <- getwd()
+setwd(pkg)
+options(covr.record_tests = TRUE)
+
+print(getOption("covr.record_tests"))
+
+message("Running package coverage")
+cov <- covr::package_coverage(".")
+
+setwd(curr_wd)
+
+message("start-covr -----")
+write.table(
+  cov,
+  file = ".covtracer_coverage_result.txt",
+  sep = "|",
+  row.names = TRUE,
+  col.names = NA,
+  na = "NA",
+  fileEncoding = "UTF-8",
+  quote = FALSE
+)
+
+print(cov)
+message("start-zero_cov -----")
+cov_percent <- covr::percent_coverage(cov)
+message("Coverage: ", cov_percent)
+zero_cov <- covr::zero_coverage(cov)
+if (nrow(zero_cov) > 0) {
+  write.table(
+    zero_cov,
+    file = ".covr_zero_coverage.txt",
+    sep = "|",
+    row.names = TRUE,
+    col.names = NA,
+    na = "NA",
+    fileEncoding = "UTF-8",
+    quote = FALSE
+  )
+}
+print(zero_cov)
+
+message("start-ttdf -----")
+message("execute covtracer::test_trace_df")
+ttdf <- test_trace_df(cov)
+write.table(
+  ttdf,
+  file = ".covtracer_ttdf.txt",
+  sep = "|",
+  row.names = TRUE,
+  col.names = NA,
+  na = "NA",
+  fileEncoding = "UTF-8",
+  quote = FALSE
+)
+print(ttdf)
+
+message("start-traceability_matrix -----")
+traceability_matrix <- ttdf %>%
+  dplyr::filter(!doctype %in% ignored_file_types) %>%
+  dplyr::select(test_name, file) %>%
+  dplyr::filter(!duplicated(.)) %>%
+  dplyr::arrange(file)
+
+write.table(
+  traceability_matrix,
+  file = ".covtracer_traceability_matrix.txt",
+  sep = "|",
+  row.names = TRUE,
+  col.names = NA,
+  na = "NA",
+  fileEncoding = "UTF-8",
+  quote = FALSE
+)
+print(traceability_matrix)
+
+message("start-untested_behaviour -----")
+untested_behaviour <- ttdf %>%
+  dplyr::filter(!doctype %in% ignored_file_types) %>% # ignore objects without testable code
+  dplyr::select(test_name, count, alias, file) %>%
+  dplyr::filter(is.na(count)) %>%
+  dplyr::arrange(alias)
+
+print(untested_behaviour)
+if (nrow(untested_behaviour) > 0) {
+  write.table(
+    untested_behaviour,
+    file = ".covtracer_untested_behaviour.txt",
+    sep = "|",
+    row.names = TRUE,
+    col.names = NA,
+    na = "NA",
+    fileEncoding = "UTF-8",
+    quote = FALSE
+  )
+}
+
+message("start-directly_tested -----")
+directly_tested <- ttdf %>%
+  dplyr::filter(!doctype %in% c("data", "class")) %>% # ignore objects without testable code
+  dplyr::select(direct, alias) %>%
+  dplyr::group_by(alias) %>%
+  dplyr::summarize(any_direct_tests = any(direct, na.rm = TRUE)) %>%
+  dplyr::arrange(alias)
+
+write.table(
+  directly_tested,
+  file = ".covtracer_directly_tested.txt",
+  sep = "|",
+  row.names = TRUE,
+  col.names = NA,
+  na = "NA",
+  fileEncoding = "UTF-8",
+  quote = FALSE
+)
+print(directly_tested)
+
+# print result of print to file
+message("start-coverage_report")
+message(paste0("Coverage: ", cov_percent))
+
+print(cov)
+if (cov_percent < minimal_coverage) {
+  warning("❌  Insufficient test coverage")
+}
+message("end-coverage_report")
+
+setwd(curr_wd)
--- README.md
+++ README.md
@@ -1,2 +1,117 @@
-# covtracer-action
+# Covtracer Action
 Github Action based on the Covtracer R package
+
+## Description
+GitHub Action based on the [Covtracer](https://github.com/Genentech/covtracer) R package.
+
+Supported R version > 4.x
+
+## Action Type
+Composite
+
+## Quick Start
+
+1. Create new action file `.github/workflows/covtracer-check.yaml` and put example content:
+
+```yaml
+---
+name: CovtracerCheck
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  covtracer:
+    runs-on: ubuntu-latest
+    name: Covtracer
+    container:
+      image: rocker/verse:4.1.2
+    steps:
+      - name: Checkout repo
+        uses: actions/checkout@v2
+
+      - name: Run rcmdcheck
+        run: |
+          R CMD build .
+          R CMD INSTALL --with-keep.source *.tar.gz
+          R CMD check *.tar.gz
+
+      - name: Run CovtracerCheck
+        uses: insightsengineering/covtracer-action@v1
+        env:
+          GITHUB_PAT: ${{ secrets.MY_PUBLIC_GITHUB_TOKEN }}
+
+```
+
+2. Create PR to test CovtracerCheck action.
+
+## Environment variables
+
+It is preferred to add secret like `MY_PUBLIC_GITHUB_TOKEN`
+for repository or organization [(Managing encrypted secrets)](https://docs.github.com/en/codespaces/managing-your-codespaces/managing-encrypted-secrets-for-your-codespaces)
+to avoid GitHub download limit being set to `unauthenticated account`. 
+
+
+* `GITHUB_PAT`:
+
+  _Description_: Github user [Personal Access Token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)
+                 - token required to read public repositories
+
+  _Required_: `false`
+
+
+## Inputs
+
+* `path`:
+
+  _Description_: Path to package's root
+
+  _Required_: `false`
+
+  _Default_: `.`
+
+* `allow-failure`:
+
+  _Description_: CovtracerCheck errors will give a warning instead of causing a pipeline failure.
+
+  _Required_: `false`
+
+  _Default_: `false`
+
+* `ignored-file-types`:
+
+  _Description_: CovtracerCheck can ignore non-code file types.
+
+  _Required_: `false`
+
+  _Default_: `data,class`
+
+* `minimal-coverage`:
+
+  _Description_: Minimal coverage threshold.
+
+  _Required_: `false`
+
+  _Default_: `80`
+
+* `post-result-as-comment`:
+
+  _Description_: Post the check result as a PR comment.
+
+  _Required_: `false`
+
+  _Default_: `false`
+
+* `no-cache`:
+
+  _Description_: Disable GitHub Action R dependency caching.
+
+  _Required_: `false`
+
+  _Default_: `false`
+
